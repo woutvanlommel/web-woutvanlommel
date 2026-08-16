e6f4998523e7bdf516919f1c5f47fa6ace2ac777
woutvanlommel <woutvanlommel@icloud.com>
2026-08-16 19:07:08 +0200

    feat: add additional skills to the SkillsService for enhanced skill representation

--- src/app/shared/skills.service.ts
+++ src/app/shared/skills.service.ts
@@ -12,16 +12,21 @@ export class SkillsService {
     { name: 'TypeScript', type: 'Frontend' },
     { name: 'JavaScript', type: 'Frontend' },
     { name: 'Angular', type: 'Frontend' },
+    { name: 'Vue.js', type: 'Frontend' },
     { name: 'React', type: 'Frontend' },
     { name: 'Vite', type: 'Frontend' },
     { name: 'Node.js', type: 'Backend' },
     { name: 'Laravel', type: 'Backend' },
     { name: 'PHP', type: 'Backend' },
     { name: 'API Development', type: 'Backend' },
+    { name: 'Python', type: 'Backend' },
     { name: 'MySQL', type: 'Backend' },
+    { name: 'PostgreSQL', type: 'Backend' },
     { name: 'Git', type: 'Tools' },
     { name: 'WordPress', type: 'Tools' },
     { name: 'Figma', type: 'Tools' },
+    { name: 'Docker', type: 'Tools' },
+    { name: 'Filament', type: 'Tools' },
   ];
 
   getFrontendSkills() {
